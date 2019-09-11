5416cdc4c0546bbcf341ac1a19b58e3c8ae5c1ff
kob-e <kobieshka@gmail.com>
2019-09-11 21:34:16 +0300

    latest

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import './App.css';
+import Form from './Form';
+import BookList from './BookList';
+
+function App() {
+    const [num, setNum] = useState(5);
+
+    const changeNumEvent = (num) => {
+        setNum(num);
+    }
+    return (
+        <main className='app'>
+            <h1>My book store</h1>
+            <Form changeNum={changeNumEvent} num={num} />
+            <BookList num={num} />
+        </main>
+    );
+}
+
+export default App;
